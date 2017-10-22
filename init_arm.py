54cb8934bc8f11266d519d8d7d69fe321e879653
sandeep <cvs.sandeep@gmail.com>
2017-10-22 14:52:44 -0700

    Updated reset_arm , display, position_creator

--- init_arm.py
+++ init_arm.py
@@ -15,4 +15,35 @@ def intialize():
 	print "Intializing moveit commander"
 	moveit_commander.roscpp_initialize(sys.argv)
 	rospy.init_node('TurtleBot_ARM',anonymous=True)
-        
+
+def position_creater(joint0,joint1,joint2,joint3,joint4):
+        #Clear pose target
+        arm.clear_pose_targets()
+
+        #Get the create set of joint values
+        group_variable_values = arm.get_current_joint_values()
+
+        group_variable_values[0] = joint0
+        group_variable_values[1] = joint1
+        group_variable_values[2] = joint2
+        group_variable_values[3] = joint3
+        group_variable_values[4] = joint4
+
+        arm.set_joint_value_target(group_variable_values)
+
+        plan = arm.plan()
+
+        arm.go(wait=True)
+
+def reset_arm():
+	set.position_creater(1.66, -1.61, -1.58, -0.48, 0.0)
+
+def display()
+	print "============ Reference frame: %s" % arm.get_planning_frame()
+	print "============ Reference frame: %s" % arm.get_planning_frame()
+	print "============ Reference frame: %s" % arm.get_end_effector_link()
+	print "============ Robot Groups:"
+	print robot.get_group_names()
+	print "============ Printing robot state"
+	print robot.get_current_state()
+	print "============"
