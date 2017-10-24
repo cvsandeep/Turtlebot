3ada8b25333c21c786a0f0d47ac905a22d97e837
Turtlebot2i <turtlebot@pdx.edu>
2017-10-23 18:56:59 -0700

    Added function for rotational motion

--- init_arm.py
+++ init_arm.py
@@ -47,7 +47,7 @@ def position_creater(joint0,joint1,joint2,joint3,joint4):
         group_variable_values[2] = joint2
         group_variable_values[3] = joint3
         group_variable_values[4] = joint4
-
+        print "Moving to desired location"
         arm.set_joint_value_target(group_variable_values)
 
         plan = arm.plan()
@@ -55,7 +55,9 @@ def position_creater(joint0,joint1,joint2,joint3,joint4):
         arm.go(wait=True)
 
 def reset_arm():
-	set.position_creater(1.66, -1.61, -1.58, -0.48, 0.0)
+    print "Moving the arm to reset position"
+    position_creater(-1.55, -1.60, -1.58, 1.50, 0.0)
+    #position_creater(1.70, -1.61, -1.58, -0.48, 0.0)
 
 def display():
 	print "============ Reference frame: %s" % arm.get_planning_frame()
@@ -68,20 +70,70 @@ def display():
 	print "============"
 
 def set_gripper(state):
-	if state == "opened":
-		rospy.loginfo("Set Gripper: Open " +  str(gripper_opened))
-        	gripper.set_joint_value_target(gripper_opened)
-        	gripper.go() 
-	if state == "close":
-                rospy.loginfo("Set Gripper: close " +  str(gripper_closed))
-                gripper.set_joint_value_target(gripper_closed)
-                gripper.go() 
-	if state == "neutral":
-                rospy.loginfo("Set Gripper: neutral " +  str(gripper_neutral))
-                gripper.set_joint_value_target(gripper_neutral)
-                gripper.go() 	
-        if state == "tighten":
-                rospy.loginfo("Set Gripper: tighten " +  str(gripper_tighten))
-                gripper.set_joint_value_target(gripper_tighten)
-                gripper.go()
-                                	
+    if state == "opened":
+              rospy.loginfo("Set Gripper: Open " +  str(gripper_opened))
+              gripper.set_joint_value_target(gripper_opened)
+              gripper.go() 
+    if state == "close":
+              rospy.loginfo("Set Gripper: close " +  str(gripper_closed))
+              gripper.set_joint_value_target(gripper_closed)
+              gripper.go() 
+    elif state == "neutral":
+              rospy.loginfo("Set Gripper: neutral " +  str(gripper_neutral))
+              gripper.set_joint_value_target(gripper_neutral)
+              gripper.go()
+    elif state == "tighten":
+              rospy.loginfo("Set Gripper: tighten " +  str(gripper_tighten))
+              gripper.set_joint_value_target(gripper_tighten)
+              gripper.go()
+                          	
+def rotation_motion(degree):
+	#Declare variable to store degree that's converted  to servo value
+	conversion = 0
+
+	#Initialize the arm
+	arm.clear_pose_targets()
+	group_variable_values = arm.get_current_joint_values
+	
+	#Case for degrees between 0 and 89
+	if 0 < degree < 89:
+		#Display information about movement
+		rospy.loginfo("Rotation is between 0 and 89 degrees")
+		
+		#Convert degrees to servo values
+		conversion = (-1.55 - (degree * 0.01177))
+
+		#Initialize the servos
+		group_variable_values[0] = conversion
+		group_variable_values[1] = -1.60
+		
+	#Case for a degree of 90
+	elif degree == 90:
+		#Display information about movement
+		rospy.loginfo("Rotation is 90 degrees")
+
+		#Initialize the servos
+		group_variable_values[0] = conversion
+		group_variable_values[1] = -1.60
+		
+	#Case for degrees between 91 and 180
+	elif 91 < degree < 180:
+		#Display information about movement
+		rospy.loginfo("Rotation is between 91 and 180 degrees")
+		
+		#Convert degrees to servo values
+		conversion = (2.61 - (degree * 0.01191))
+
+		#Initialize the servos
+		group_variable_values[0] = conversion
+		group_variable_values[1] = -1.60
+
+	#Case for degrees that are out of bounds
+	else:
+		#Display information about movement
+		rospy.loginfo("Error: Rotation is out of bounds")
+
+	#Move arm to the desired position(degree)
+	arm.set_joint_value_target(group_variable_values)
+	plan = arm.plan()
+	arm.go()
