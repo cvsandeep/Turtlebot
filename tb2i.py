6a01e43252f942b7ee8960450de1034a0c28b6fe
Turtlebot2i <turtlebot@pdx.edu>
2017-10-25 18:14:04 -0700

    Added lift_percentage function to the tb2i and test

--- tb2i.py
+++ tb2i.py
@@ -198,3 +198,39 @@ class tb_arm:
         tb_arm.arm.set_joint_value_target(group_variable_values)
         plan = tb_arm.arm.plan()
         tb_arm.arm.go(wait=True)
+
+def lift_percentage(percentage):
+	# Initialize the arm
+        # Clear pose target
+        tb_arm.arm.clear_pose_targets()
+        # Get the current set of joint values
+        group_variable_values = tb_arm.arm.get_current_joint_values() 
+
+	# Declare variables 
+	ouput = 0
+
+	# If percentage is between 0 and 49
+	if 0 <= percentage and percentage <= 49:
+		
+		# Convert to servo value
+    		ouput = -1.13 + (percentage * 0.022)
+   	 	print ("The output is:", ouput)
+		group_variable_values[1] = output
+
+	#If percentage is between 50 and 100
+  	elif 50 <= percentage and percentage <= 100:
+		
+		# Convert to servo value
+    		ouput = ((percentage - 50) * 0.011)
+    		print ("The output is:", ouput)
+		group_variable_values[1] = output
+
+	#Otherwise the percentage is invalid
+  	else:
+		# Display percentage error
+    		print ("This is an invalid state")
+
+	# Rotate the arm to the desired location
+        tb_arm.arm.set_joint_value_target(group_variable_values)
+        plan = tb_arm.arm.plan()
+        tb_arm.arm.go(wait=True)
